8a533e5fde50ff4f81579a6f7b9fe0b9eef1ae08
jubrannassar <juby.nassar123@gmail.com>
2021-08-12 11:37:34 -0400

    creates services folder

---
+++ client/src/services/projects.js
@@ -0,0 +1,26 @@
+import api from "./apiConfig"
+
+export const getAllProjects = async => {
+  const res = await api.get("/projects")
+  return res.data
+}
+
+export const getOneProject = async projectID => {
+  const res = await api.get(`/projects/${projectID}`)
+  return res.data
+}
+
+export const deleteProject = async projectID => {
+  await api.delete(`/project/${projectID}`)
+}
+
+export const addProject = async projectData=> {
+  const res = await api.post("/projects", { project: projectData })
+  return res.data
+}
+
+export const updateProject = async (projectID, projectData) => {
+  const res = await api.put(`/projects/${projectID}`, { project: projectData})
+  return res.data
+}
+
---
+++ client/src/services/skills.js
@@ -0,0 +1,6 @@
+import api from "./apiConfig"
+
+export const getAllSkills = async => {
+  const res = await api.get("/skills")
+  return res.data
+}
