c1c797e15acb885b7f5d2698957e0c71f2707471
jubrannassar <juby.nassar123@gmail.com>
2021-08-12 12:04:40 -0400

    creates home screen

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from "../../components/Nav/Nav.jsx";
+import AboutMe from "../../components/AboutMe/AboutMe.jsx";
+import Projects from "../../components/Projects/Projects.jsx";
+import Contact from "../../components/Contact/Contact.jsx";
+
+export default Home = () => {
+  <>
+    <Nav />
+    <AboutMe />
+    <Projects />
+    <Contact />
+  </>;
+}
