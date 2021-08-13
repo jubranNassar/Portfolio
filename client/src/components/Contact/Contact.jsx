2b149b6990647d5b6f51d7e094b4c727c64e7a14
jubrannassar <juby.nassar123@gmail.com>
2021-08-13 17:52:26 -0400

    creates contact me page

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,10 +1,51 @@
-
-
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router";
+import { createMessage } from "../../services/contacts";
 
 export default function Contact() {
+  const [contact, setContact] = useState({
+    email: "",
+    message: "",
+  });
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setContact({
+      ...setContact,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.prevent.default();
+    await createMessage(contact);
+    history.push("/");
+  };
+
   return (
-    <div id="contact">
-      
+    <div className="contact">
+      <h1>Contact Me</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={(e) => handleChange(e)}
+          id="email"
+          name="email"
+          type="email"
+          value={contact.email}
+        />
+        <label htmlFor="message">Your kind words...</label>
+        <input
+          onChange={(e) => handleChange(e)}
+          id="message"
+          name="message"
+          type="text"
+          value={contact.message}
+        />
+        <button type="submit">Send!</button>
+      </form>
     </div>
-  )
+  );
 }
