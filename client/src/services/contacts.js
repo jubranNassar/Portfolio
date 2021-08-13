864f5593817d31e983a80feee931f158d55af132
jubrannassar <juby.nassar123@gmail.com>
2021-08-13 15:22:48 -0400

    creates routes for contacts db

---
+++ client/src/services/contacts.js
@@ -0,0 +1,21 @@
+import api from "./apiConfig";
+
+export const getAllMessages = async()=> {
+  const res = await api.get("/contacts")
+  return res.data
+}
+
+export const getOneMessage = async(id) => {
+  const res = await api.get(`/contacts/${id}`)
+  return res.data
+}
+
+export const deleteMessage = async(id) => {
+  await api.delete(`/contacts/${id}`)
+}
+
+export const createMessage = async(contactData) => {
+  const res = await api.post("/contacts", { contact: contactData})
+  return res.data
+}
+
