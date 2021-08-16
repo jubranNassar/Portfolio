c610588d12cfde016a3b2528668b4a9ee68eedbf
jubrannassar <juby.nassar123@gmail.com>
2021-08-16 01:17:04 -0400

    adds css to login page

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,3 +1,5 @@
+import "./Login.css";
+
 export default function Login({ handleLogin, loginForm, setLoginForm }) {
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -7,23 +9,31 @@ export default function Login({ handleLogin, loginForm, setLoginForm }) {
     }));
   };
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={(e) => handleLogin(e)}>
-        <label htmlFor="">Username</label>
-        <input
-          onChange={(e) => handleChange(e)}
-          type="text"
-          name="username"
-          value={loginForm.username}
-        />
-        <label htmlFor="">password</label>
-        <input
-          onChange={(e) => handleChange(e)}
-          type="password"
-          name="password"
-          value={loginForm.password}
-        />
+    <div className="login">
+      <h1 className="login-title">Welcome Back</h1>
+      <form className="login-form" onSubmit={(e) => handleLogin(e)}>
+        <div className="login-label-input">
+          <div className="login-label">
+            <label htmlFor="">Username</label>
+          </div>
+          <input
+            onChange={(e) => handleChange(e)}
+            type="text"
+            name="username"
+            value={loginForm.username}
+          />
+        </div>
+        <div className="login-label-input">
+          <div className="login-label">
+            <label htmlFor="">Password</label>
+          </div>
+          <input
+            onChange={(e) => handleChange(e)}
+            type="password"
+            name="password"
+            value={loginForm.password}
+          />
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
