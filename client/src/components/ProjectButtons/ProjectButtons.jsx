0236c47fed893fa28bc01d2e177d655f7b927ea6
jubrannassar <juby.nassar123@gmail.com>
2021-08-16 11:52:32 -0400

    fixes buttons bug

--- client/src/components/ProjectButtons/ProjectButtons.jsx
+++ client/src/components/ProjectButtons/ProjectButtons.jsx
@@ -14,7 +14,7 @@ export default function ProjectButtons({
       </Link>
 
       <Link to={{ pathname: githubURL }} target="_blank">
-        <button className="button">See the code</button>
+        <button onClick={console.log("HELLOO")} className="button">See the code</button>
       </Link>
     </>
   );
--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects({ user }) {
     fetchProjects();
   }, []);
   return (
-    <div data-aos="fade-left" id="projects">
+    <div id="projects">
       <h1 className="projects-title">My Projects</h1>
       <div id="my-projects">
       {projects.map((project) => (
