5f886a9d7f16373d6173348a19c28fa28202277c
jubrannassar <juby.nassar123@gmail.com>
2021-08-16 22:25:50 -0400

    adds favicon and fixes media queries

--- client/src/screens/AddProject/AddProject.jsx
+++ client/src/screens/AddProject/AddProject.jsx
@@ -35,60 +35,72 @@ export default function AddProject({ user }) {
       <h1 className="add-title">Add Project</h1>
       <form className="add-form" onSubmit={(e) => handleSubmit(e)}>
         <div className="add-label-input">
-        <label className="add-label" htmlFor="name">Name</label>
-        <input
-          onChange={(e) => handleChange(e)}
-          id="name"
-          name="name"
-          type="text"
-          value={formData.name}
-          className="add-input"
-        />
+          <label className="add-label" htmlFor="name">
+            Name
+          </label>
+          <input
+            onChange={(e) => handleChange(e)}
+            id="name"
+            name="name"
+            type="text"
+            value={formData.name}
+            className="add-input"
+          />
         </div>
         <div className="add-label-input">
-        <label className="add-label" htmlFor="img">Image URL</label>
-        <input
-          onChange={(e) => handleChange(e)}
-          id="img"
-          name="image"
-          type="text"
-          value={formData.image}
-          className="add-input"
-        />
+          <label className="add-label" htmlFor="img">
+            Image URL
+          </label>
+          <input
+            onChange={(e) => handleChange(e)}
+            id="img"
+            name="image"
+            type="text"
+            value={formData.image}
+            className="add-input"
+          />
         </div>
         <div className="add-label-input">
-        <label className="add-label" htmlFor="gh-url">github URL</label>
-        <input
-          onChange={(e) => handleChange(e)}
-          id="gh-url"
-          name="github_url"
-          type="text"
-          value={formData.github_url}
-          className="add-input"
-        />
+          <label className="add-label" htmlFor="gh-url">
+            github URL
+          </label>
+          <input
+            onChange={(e) => handleChange(e)}
+            id="gh-url"
+            name="github_url"
+            type="text"
+            value={formData.github_url}
+            className="add-input"
+          />
         </div>
         <div className="add-label-input">
-        <label className="add-label" htmlFor="deployed-url">Deployed URL</label>
-        <input
-          onChange={(e) => handleChange(e)}
-          id="deployed-url"
-          name="deployed_url"
-          type="text"
-          value={formData.deployed_url}
-          className="add-input"
-        />
+          <label className="add-label" htmlFor="deployed-url">
+            Deployed URL
+          </label>
+          <input
+            onChange={(e) => handleChange(e)}
+            id="deployed-url"
+            name="deployed_url"
+            type="text"
+            value={formData.deployed_url}
+            className="add-input"
+          />
         </div>
         <div className="add-label-input">
-        <label className="add-label" htmlFor="desc">Description</label>
-        <textarea
-          onChange={(e) => handleChange(e)}
-          id="desc"
-          name="description"
-          type="text"
-          value={formData.description}
-          className="add-input"
-        />
-        <button className="add-button" type="submit">Submit</button>
+          <label className="add-label" htmlFor="desc">
+            Description
+          </label>
+          <textarea
+            onChange={(e) => handleChange(e)}
+            id="desc"
+            name="description"
+            type="text"
+            value={formData.description}
+            className="add-input"
+          />
+          <button className="add-button" type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
