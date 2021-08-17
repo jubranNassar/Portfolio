c1bfe074a7f0ff2ff4ed78bf42ada6df396620b0
jubrannassar <juby.nassar123@gmail.com>
2021-08-17 14:00:53 -0400

    adds animtion to navbar links

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -5,9 +5,10 @@ export default function Landing() {
     <div id="landing">
       <h1 className="intro">
         <span className="greeting">Hi, my name is Jubran.</span> <br />
-        <span className="greeting1">I'm a Software
-        <br />
-        Engineer.
+        <span className="greeting1">
+          I'm a Software
+          <br />
+          Engineer.
         </span>
       </h1>
     </div>
