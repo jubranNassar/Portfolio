b8617b92a89d856b32258b54c8e1eeeb5c86ce1c
jubrannassar <juby.nassar123@gmail.com>
2021-08-20 19:08:16 -0400

    adds css to contacts page

--- client/src/screens/Messages/Messages.jsx
+++ client/src/screens/Messages/Messages.jsx
@@ -1,33 +1,35 @@
-import { getAllMessages } from "../../services/contacts";
 import { useState, useEffect } from "react";
+import { getAllMessages, deleteMessage } from "../../services/contacts";
 import "./Messages.css";
 
+export default function Messages({ user }) {
+  const [message, setMessage] = useState([]);
+  const [toggleFetch, setToggleFetch] = useState(false);
 
-export default function Messages({user}) {
-  const [message, setMessage] = useState([])
-
-  useEffect(()=> {
-    const fetchMessages = async ()=> {
+  useEffect(() => {
+    const fetchMessages = async () => {
       const allMessages = await getAllMessages();
-      setMessage(allMessages)
-    }
+      setMessage(allMessages);
+    };
     fetchMessages();
-  }, []);
+  }, [toggleFetch]);
+
+  const handleDelete = async (id) => {
+    await deleteMessage(id);
+    setToggleFetch(!toggleFetch);
+  };
 
   const loggedIn = (
     <div className="contacts">
-    <h1>Messages</h1>
-    {message.map((message)=> (
-      <div key={message.id}>
-        <h1>From: {message.email}</h1>
-        <p>{message.message}</p>
-      </div>
-    ))}
-  </div>
-  )
-  return (
-    <>
-    {user ? loggedIn : null}
-    </>
-  )
+      <h1 className="contact-title">Messages</h1>
+      {message.map((message) => (
+        <div className="contact-card" key={message.id}>
+          <h1>From: {message.email}</h1>
+          <p>{message.message}</p>
+          <button onClick={() => handleDelete(message.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+  return <>{user ? loggedIn : null}</>;
 }
