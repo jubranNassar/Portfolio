8de84d383605718fed71023e26e303207e90186c
jubrannassar <juby.nassar123@gmail.com>
2021-08-20 22:01:30 -0400

    adds restrictions on edit page

--- client/src/screens/EditProject/EditProject.jsx
+++ client/src/screens/EditProject/EditProject.jsx
@@ -47,86 +47,99 @@ export default function EditProject({ user }) {
     history.push("/");
   };
 
-  return (
+  const loggedIn = (
     <div className="edit">
-      <h1 className="edit-title">Edit Project</h1>
-      <form className="edit-form" onSubmit={(e) => handleSubmit(e)}>
-        <div className="edit-label-input">
-          <label className="edit-label" htmlFor="name">
-            Name
-          </label>
-          <input
-            onChange={(e) => handleChange(e)}
-            id="name"
-            name="name"
-            type="text"
-            value={formData.name}
-            className="edit-input"
-          />
-        </div>
-        <div className="edit-label-input">
-          <label className="edit-label" htmlFor="img">
-            Image URL
-          </label>
-          <input
-            onChange={(e) => handleChange(e)}
-            id="img"
-            name="image"
-            type="text"
-            value={formData.image}
-            className="edit-input"
-          />
-        </div>
-        <div className="edit-label-input">
-          <label className="edit-label" htmlFor="gh-url">
-            github URL
-          </label>
-          <input
-            onChange={(e) => handleChange(e)}
-            id="gh-url"
-            name="github_url"
-            type="text"
-            value={formData.github_url}
-            className="edit-input"
-          />
-        </div>
-        <div className="edit-label-input">
-          <label className="edit-label" htmlFor="deployed-url">
-            Deployed URL
-          </label>
-          <input
-            onChange={(e) => handleChange(e)}
-            id="deployed-url"
-            name="deployed_url"
-            type="text"
-            value={formData.deployed_url}
-            className="edit-input"
-          />
-        </div>
-        <div className="edit-label-input">
-          <label className="edit-label" htmlFor="desc">
-            Description
-          </label>
-          <textarea
-            onChange={(e) => handleChange(e)}
-            id="desc"
-            name="description"
-            type="text"
-            value={formData.description}
-            className="edit-input"
-          />
-        </div>
-        <button className="edit-button" type="submit">
-          Submit
-        </button>
-      </form>
-      <button
-        className="edit-button"
-        onClick={(e) => handleDelete(e)}
-        type="submit"
-      >
-        Delete Project
+    <h1 className="edit-title">Edit Project</h1>
+    <form className="edit-form" onSubmit={(e) => handleSubmit(e)}>
+      <div className="edit-label-input">
+        <label className="edit-label" htmlFor="name">
+          Name
+        </label>
+        <input
+          onChange={(e) => handleChange(e)}
+          id="name"
+          name="name"
+          type="text"
+          value={formData.name}
+          className="edit-input"
+        />
+      </div>
+      <div className="edit-label-input">
+        <label className="edit-label" htmlFor="img">
+          Image URL
+        </label>
+        <input
+          onChange={(e) => handleChange(e)}
+          id="img"
+          name="image"
+          type="text"
+          value={formData.image}
+          className="edit-input"
+        />
+      </div>
+      <div className="edit-label-input">
+        <label className="edit-label" htmlFor="gh-url">
+          github URL
+        </label>
+        <input
+          onChange={(e) => handleChange(e)}
+          id="gh-url"
+          name="github_url"
+          type="text"
+          value={formData.github_url}
+          className="edit-input"
+        />
+      </div>
+      <div className="edit-label-input">
+        <label className="edit-label" htmlFor="deployed-url">
+          Deployed URL
+        </label>
+        <input
+          onChange={(e) => handleChange(e)}
+          id="deployed-url"
+          name="deployed_url"
+          type="text"
+          value={formData.deployed_url}
+          className="edit-input"
+        />
+      </div>
+      <div className="edit-label-input">
+        <label className="edit-label" htmlFor="desc">
+          Description
+        </label>
+        <textarea
+          onChange={(e) => handleChange(e)}
+          id="desc"
+          name="description"
+          type="text"
+          value={formData.description}
+          className="edit-input"
+        />
+      </div>
+      <button className="edit-button" type="submit">
+        Submit
       </button>
+    </form>
+    <button
+      className="edit-button"
+      onClick={(e) => handleDelete(e)}
+      type="submit"
+    >
+      Delete Project
+    </button>
+  </div>
+  )
+
+  const notLoggedIn = (
+    <div className="not-logged-in">
+      <h1>Sorry, Not found.</h1>
     </div>
+  )
+
+  return (
+    <>
+    {user ? loggedIn : notLoggedIn }
+    </>
+
   );
 }
