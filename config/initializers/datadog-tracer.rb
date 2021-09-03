7e189a5d42e9310f4ce3b75a16ea1cb2df2c13dc
jubrannassar <juby.nassar123@gmail.com>
2021-09-02 20:28:00 -0400

    changes dd tracer file

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,5 +1,4 @@
 Datadog.configure do |c|
-  c.use :rails, service_name: 'heroku-app'
-  # c.env = 'portfolio-api'
-  # c.service = 'heroku-app'
+  c.env = 'portfolio-api'
+  c.service = 'heroku-app'
 end
