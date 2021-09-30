b7f6ecc5a194d56a8f9975cb35f636d30ee70b3c
jubrannassar <juby.nassar123@gmail.com>
2021-09-30 15:27:44 -0400

    adds routes to skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,44 @@
 class SkillsController < ApplicationController
+  before_action :get_skill, only: %i[update destroy]
+  before_action :authorize_request, only: %i[create update destroy]
+
   def index
     render json: Skill.all
+  end
+
+  def create
+    skill = Skill.new(skill_params)
+    if skill.save
+      render json: skill
+    else
+      render json: 'Error, try again'
+    end
+  end
+
+  def update
+    if @skill.update(skill_params)
+      render json: @project
+    else
+      render json: 'Error, try again'
+
+    end
+  end
+
+  def destroy
+    if @skill.destroy
+      render json: 'Deleted'
+    else
+      render json: 'Error, please try again'
+    end
+  end
+
+  private
+
+  def skill_params
+    params.require(:skill).permit(:name, :image_url)
+  end
+
+  def get_skill
+    @skill = Skill.find(params[:id])
   end
 end
